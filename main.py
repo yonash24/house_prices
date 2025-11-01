3ecf56300e1d669456b61c5c39a7daf5378f28cd
yona <yonashay87@gmail.com>
2025-11-01 12:32:47 +0200

    analize the data

---
+++ main.py
@@ -0,0 +1,24 @@
+from DataHandler import ImportData, DataInfo
+
+
+
+
+
+def main():
+    ### import the data ###
+    ImportData.import_data()
+    data_dict = ImportData.to_df()
+
+    ### analyz the data ###
+    analyzer = DataInfo(data_dict)
+    #analyzer.salePrise_info()
+    #analyzer.log_distributoin()
+    #analyzer.get_missing_vals()
+    #analyzer.features_coreleation()
+    #analyzer.features_scatter("BedroomAbvGr", "HalfBath")
+    #analyzer.categorical_features_analysis()
+
+
+
+if __name__ == "__main__":
+    main()
